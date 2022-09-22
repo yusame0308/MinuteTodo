46f78d2c098f1257d6b9afeb66893cf1015fe184
yusame <yu.starts.yu@gmail.com>
2022-09-22 12:26:40 +0900

    edit .gitignore, add ViewControllers

---
+++ DailyPlan/ViewControllers/PlanViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlanViewController.swift
+//  DailyPlan
+//
+//  Created by 小原宙 on 2022/09/22.
+//
+
+import UIKit
+
+class PlanViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+    
+}
