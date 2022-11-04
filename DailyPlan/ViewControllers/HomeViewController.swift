6136e4a32fc116c424c54227d159174a0114c976
yusame <yu.starts.yu@gmail.com>
2022-11-04 13:18:08 +0900

    カスタムセルのデザインを設定

--- DailyPlan/ViewControllers/HomeViewController.swift
+++ DailyPlan/ViewControllers/HomeViewController.swift
@@ -11,14 +11,15 @@ class HomeViewController: UIViewController {
     
     private let cellId = "cellId"
     
-    var exampleArray: [String] = ["0番上のセル", "1番目のセル", "2番目のセル"]
+    var titles: [String] = ["0番上のセル", "セル", "2番目のセルテストテストあああああああああああああああああああ"]
+    var times: [Int] = [30, 40, 120]
+    var limits: [String] = ["11/1", "11/12", "11/25"]
     
     lazy var taskTableView: UITableView = {
         let tv = UITableView()
         tv.delegate = self
         tv.dataSource = self
-        tv.rowHeight = 70
-        tv.separatorColor = UIColor.blue
+        tv.separatorStyle = .none
         tv.register(TaskTableViewCell.self, forCellReuseIdentifier: cellId)
         return tv
     }()
@@ -41,12 +42,12 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: UITableViewDelegate,UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return exampleArray.count
+        return titles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! TaskTableViewCell
-        cell.textLabel?.text = exampleArray[indexPath.row]
+        cell.setCell(title: titles[indexPath.row], time: times[indexPath.row], limit: limits[indexPath.row])
         
         return cell
     }
--- DailyPlan/Views/Home/TaskTableViewCell.swift
+++ DailyPlan/Views/Home/TaskTableViewCell.swift
@@ -8,4 +8,54 @@
 import UIKit
 
 class TaskTableViewCell: UITableViewCell {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    let limitLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(titleLabel)
+        addSubview(timeLabel)
+        addSubview(limitLabel)
+        
+        titleLabel.anchor(top: self.topAnchor, left: self.leftAnchor, topPadding: 10, leftPadding: 30)
+        timeLabel.anchor(top: titleLabel.bottomAnchor, bottom: self.bottomAnchor, left: titleLabel.leftAnchor, bottomPadding: 10)
+        limitLabel.anchor(left: titleLabel.rightAnchor, right: self.rightAnchor, centerY: self.centerYAnchor, width: 50, leftPadding: 10, rightPadding: 20)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setCell(title: String, time: Int, limit: String) {
+        titleLabel.text = title
+        timeLabel.text = String(time)
+        limitLabel.text = limit
+    }
+    
 }
