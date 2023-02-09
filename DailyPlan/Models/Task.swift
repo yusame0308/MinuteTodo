e3d0c254185c6a06c28d4acb465096be61ded427
yusame <yu.starts.yu@gmail.com>
2023-02-09 18:08:23 +0900

    Taskモデルを作成

---
+++ DailyPlan/Models/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DailyPlan
+//
+//  Created by 小原宙 on 2023/02/09.
+//
+
+import Foundation
+import RealmSwift
+
+class Task: Object {
+    @objc dynamic var title = ""
+    @objc dynamic var length = 0
+    @objc dynamic var limit = Date()
+}
