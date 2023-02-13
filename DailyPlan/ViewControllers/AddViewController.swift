c94a82ae97f522c35aa9d1252fc2a37f364ed716
yusame <yu.starts.yu@gmail.com>
2023-02-13 16:42:03 +0900

    minuteLabelを作成

--- DailyPlan/ViewControllers/AddViewController.swift
+++ DailyPlan/ViewControllers/AddViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 class AddViewController: UIViewController {
     let titleTextField = AddTextField(placeHolder: "タイトル")
     let lengthTextField = AddTextField(placeHolder: "所要時間", type: .numberPad, fontSize: 15)
+    let minuteLabel: UILabel = {
+        let label = UILabel()
+        label.text = "分"
+        label.textColor = .systemGray3
+        label.font = .systemFont(ofSize: 16)
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,9 +34,11 @@ class AddViewController: UIViewController {
         baseStackView.spacing = 20
         
         view.addSubview(baseStackView)
+        view.addSubview(minuteLabel)
         
         titleTextField.anchor(left: baseStackView.leftAnchor, right: baseStackView.rightAnchor, height: 40)
         lengthTextField.anchor(width: 80, height: 30)
+        minuteLabel.anchor(bottom: lengthTextField.bottomAnchor, left: lengthTextField.rightAnchor, bottomPadding: 4, leftPadding: 4)
         baseStackView.anchor(left: view.leftAnchor, right: view.rightAnchor, centerY: view.centerYAnchor, leftPadding: 60, rightPadding: 60)
     }
 }
