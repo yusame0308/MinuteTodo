0e2ddee93a427f6be1d4e06dab43c30d7c50d4f2
yusame <yu.starts.yu@gmail.com>
2023-02-19 16:40:22 +0900

    LimitToolBarを追加

--- DailyPlan/Views/Add/LimitToolBar.swift
+++ DailyPlan/Views/Add/LimitToolBar.swift
@@ -5,4 +5,28 @@
 //  Created by 小原宙 on 2023/02/19.
 //
 
-import Foundation
+import UIKit
+
+class LimitToolBar: UIToolbar {
+    
+    let spaceItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+    let doneItem: UIBarButtonItem = {
+        let button = UIButton()
+        button.setTitle("完了", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addAction(UIAction { [self] _ in
+        }, for: .primaryActionTriggered)
+        return UIBarButtonItem(customView: button)
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setItems([spaceItem,doneItem], animated: true)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
