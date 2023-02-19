88861c6d73628f381dfa9fee20691686215ed0df
yusame <yu.starts.yu@gmail.com>
2023-02-19 16:39:54 +0900

    LimitTextFieldを追加

--- DailyPlan/Views/Add/LimitTextField.swift
+++ DailyPlan/Views/Add/LimitTextField.swift
@@ -5,4 +5,28 @@
 //  Created by 小原宙 on 2023/02/19.
 //
 
-import Foundation
+import UIKit
+
+class LimitTextField: AddTextField {
+    
+    let limitDatePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.preferredDatePickerStyle = .compact
+        picker.datePickerMode = .date
+        picker.locale = Locale(identifier: "ja_JP")
+        picker.timeZone = TimeZone(identifier: "Asia/Tokyo")
+        return picker
+    }()
+
+    init() {
+        super.init(placeHolder: "期限", fontSize: 16)
+        
+        inputView = limitDatePicker
+        inputAccessoryView = LimitToolBar()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
