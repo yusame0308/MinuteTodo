6e5e20d16ac642722f701c79776c52a021203fe2
yusame <yu.starts.yu@gmail.com>
2023-02-23 04:52:40 +0900

    addButtonを追加

--- DailyPlan/Extensions/UIButton-Extension.swift
+++ DailyPlan/Extensions/UIButton-Extension.swift
@@ -11,7 +11,20 @@ extension UIButton {
     
     func createClearButton(action: @escaping () -> Void) -> UIButton {
         self.setImage(UIImage(systemName: "xmark.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 15)), for: .normal)
-        self.tintColor = .systemGray3
+        self.tintColor = .systemGray4
+        self.addAction(UIAction { _ in
+            action()
+        }, for: .primaryActionTriggered)
+        return self
+    }
+    
+    func createSimpleButton(title: String, action: @escaping () -> Void) -> UIButton {
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(UIColor.white, for: .normal)
+        self.setTitleColor(UIColor.systemGray5, for: .highlighted)
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        self.backgroundColor = .systemGray4
+        self.layer.cornerRadius = 5
         self.addAction(UIAction { _ in
             action()
         }, for: .primaryActionTriggered)
--- DailyPlan/ViewControllers/AddViewController.swift
+++ DailyPlan/ViewControllers/AddViewController.swift
@@ -22,6 +22,9 @@ class AddViewController: UIViewController {
         self.limitTextField.text = ""
         self.changeClearButtonAlpha()
     }
+    let addButton = UIButton().createSimpleButton(title: "追加") {
+        print("add")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,24 +45,42 @@ class AddViewController: UIViewController {
         view.addSubview(baseStackView)
         view.addSubview(minuteLabel)
         view.addSubview(clearButton)
+        view.addSubview(addButton)
         
         minuteLabel.alpha = 0
         clearButton.alpha = 0
+        addButton.alpha = 0.4
+        addButton.isEnabled = false
         
         titleTextField.anchor(left: baseStackView.leftAnchor, right: baseStackView.rightAnchor, height: 40)
         lengthTextField.anchor(width: 80, height: 30)
         minuteLabel.anchor(left: lengthTextField.rightAnchor, centerY: lengthTextField.centerYAnchor, leftPadding: 3)
         limitTextField.anchor(height: 30)
         clearButton.anchor(left: limitTextField.rightAnchor, centerY: limitTextField.centerYAnchor, leftPadding: 3)
+        addButton.anchor(top: baseStackView.bottomAnchor, centerX: view.centerXAnchor, width: 80, height: 40, topPadding: 40)
         baseStackView.anchor(left: view.leftAnchor, right: view.rightAnchor, centerY: view.centerYAnchor, leftPadding: 60, rightPadding: 60)
     }
     
     private func setupActions() {
+        
+        titleTextField.addAction(UIAction { _ in
+            UIView.animate(withDuration: 0.3, delay: 0.0, options: [.curveEaseInOut], animations: {
+                if self.titleTextField.isEmpty {
+                    self.addButton.alpha = 0.4
+                    self.addButton.isEnabled = false
+                } else {
+                    self.addButton.alpha = 1.0
+                    self.addButton.isEnabled = true
+                }
+            }, completion: nil)
+        }, for: .editingChanged)
+        
         lengthTextField.addAction(UIAction { _ in
             UIView.animate(withDuration: 0.3, delay: 0.2, options: [.curveEaseInOut], animations: {
                 self.minuteLabel.alpha = self.lengthTextField.isEmpty ? 0 : 1
             }, completion: nil)
         }, for: .editingChanged)
+        
     }
     
     private func changeClearButtonAlpha() {
